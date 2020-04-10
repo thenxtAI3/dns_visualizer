d2ba854750cb4c693f8e9bbfac412fb95d2557a8
Cean Manznao <ceanmanzano03@gmail.com>
2020-04-10 02:40:53 -0700

    messing with some scripts to start formatting output

--- run.sh
+++ run.sh
@@ -1,15 +1,36 @@
-read -p "Begin process? [Enter]"
+#!/bin/bash
+
+#read -p "Begin process? [Enter]"
+#clear
+#
+#echo "Parsing input file..."
+#cat temp.txt
+#
+#echo
+#read -p "File parsed. Run python? [Enter]"
+#clear
+#
+#python test.py
+#
+#echo
+#read -p "Python complete. Good day! [Enter]"
+#clear
+
+echo "input testing"
+read -p "Enter the site you would like to dig: " query
+
 clear
+rm -f output.txt
 
-echo "Parsing input file..."
-cat temp.txt
+echo "Digging the website url: $query"
 
-echo
-read -p "File parsed. Run python? [Enter]"
-clear
+for recordType in A AAAA; do
+    dig $ recordType $query +noadditional +noquestion +nocomments +nocmd +nostats >> output.txt
+#    dig $recordType $query | tail -n +14 > temp.txt
+#    (( count = $(wc -l < temp.txt) - 6 ))
+#    head -n $count temp.txt >> output.txt
+#     >> output.txt
+done
+rm -f temp.txt
 
-python test.py
-
-echo
-read -p "Python complete. Good day! [Enter]"
-clear
+open output.txt
