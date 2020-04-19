5da6f3e0e83dd0256a97abfabe40ddf4901b231a
sofimarquez22 <sofiamarquez@Sofias-Air-2.attlocal.net>
2020-04-19 16:20:31 -0700

    merged my code with Cean's node classes, added a line to read json as a python dictionary, and changed the yaxis to plot addresses

--- matPlot.py
+++ matPlot.py
@@ -1,22 +1,48 @@
+#!/usr/bin/env python
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import math
+import json
+import ipaddress
 
-#index_col treats the first column of the dataset as the id, written as a backup
-df = pd.read_csv('backup.txt', index_col=0, encoding= 'unicode_escape')
 
-#reading from a data file, cross your fingers and hope it works
-#df = pd.read_csv('temp.txt', delimiter= '\s+', index_col = 1, skiprows=[0,1,2,3,17,32,33,34,35,36], encoding= 'unicode_escape')
+class Node:
+    def __init__(self, id, stats):
+        self.id = id
+        self.stats = stats
+class NodeList:
+    def __init__(self, path_to_file):
+        self.path_to_file = path_to_file
+        self.nodes = []
+    def initializeList(self):
+        #reads from file and puts into a dictionary
+        #with open('test.json') as json_file:
+            #data = json.load(json_file.read())
+        #n1 = Node(1, "I am the first node")
+        #self.node.append(n1)
+        print("Type")
+    def getNodeIds(self):
+        ids = [] #ip addresses
+        for node in self.nodes:
+            ids.append(node.id)
+            return ids
+    def size(self):
+        return len(self.nodes)
+
+df = pd.read_csv('backup.txt', encoding= 'unicode_escape')
 
 sns.set(style="white")
-#sns_plot = sns.lmplot(x='Attack', y='Defense', data=df, fit_reg=False)
-
 serverImage = OffsetImage(plt.imread('server.png'), zoom=0.05)
+
+for i, row in df.iterrows():
+    ip = ipaddress.ip_address(df.loc[i,"Address"])
+    df.loc[i,"Address"]  = int(ip)
+#print(df.columns)
 x = df["Step"]
-y = df["Time"]
+y = df["Address"]
 fig, ax = plt.subplots()
 ax.scatter(x, y, marker="None")
 ax.plot(x, y)
@@ -29,17 +55,14 @@ for x0, y0 in zip(x, y):
 #    ax.annotate(s='', xy = (x0, y0), arrowprops=dict(arrowstyle="->"))
     
 #formatting
-ax.set_xlabel('Steps')
-ax.set_ylabel('Time(ms)')
-plt.ylim(0,200)
+ax.set_xlabel('Node Index') #order read
+ax.set_ylabel('Node ID') #ip address
+#plt.ylim(0,200)
 xint = range(min(x)-1, math.ceil(max(x))+2)
 plt.xticks(xint)
 
-#plt.quiver(x[:-1], y[:-1], x[1:]-x[:-1], y[1:]-y[:-1], y[1:]-x[:-1], scale_units='xy', angles='xy', scale=1)
+#textstr = 'IP:192.168.1.1\nType:Server\nRuntime:118ms'
+#props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+#ax.text(0.27, 0.63, textstr, transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=props)
 
-textstr = 'IP:192.168.1.1\nType:Server\nRuntime:118ms'
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-ax.text(0.27, 0.63, textstr, transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=props)
-
-#sns_plot.savefig("output.png")
 fig.savefig("output.png")
